600873b2691f64ef7ecba888f1f0503460266feb
Khánh Nguyễn <meikahn0109@gmail.com>
2025-12-19 00:38:21 +0700

    Thiết kế xong Modal Dialog Thoát

--- app/src/main/java/ntu/edu/nguyenquockhanh/btl_quizzing/model/Score.java
+++ app/src/main/java/ntu/edu/nguyenquockhanh/btl_quizzing/model/Score.java
@@ -1,4 +1,27 @@
 package ntu.edu.nguyenquockhanh.btl_quizzing.model;
 
 public class Score {
+    String name;
+    int score;
+
+    public Score(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
